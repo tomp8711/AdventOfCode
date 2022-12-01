8d44818eed788c3f49f5e6d560ed9da677f0765f
Tom P <tomp8711@gmail.com>
2022-12-01 15:09:25 -0500

    day 1

--- 2022/Day1/main.py
+++ 2022/Day1/main.py
@@ -1,11 +1,14 @@
 #DAY 1 Calorie Counting
 
+import functools
+
 def readFile(fileName: str) -> list:
     listInput = []
-    with open("input.txt") as file:
+    with open(fileName) as file:
         for line in file:
-            listInput.append(line.rstrip())
+            listInput.append(line.strip())
     return listInput
+
 
 def getMax(listInput: list) -> int:
     
@@ -33,11 +36,35 @@ def getThreeMax(listInput: list) -> int:
     maxCalories = sorted(caloriesList, reverse=True)[:3]
     return  sum(maxCalories) 
 
+#better solution 
+def solveReduce():
+    with open("input.txt") as file:
+        data = file.read().strip().split("\n\n")
+        sumsV = []
+        for part in data:
+           # print(part.split())
+            val = int(functools.reduce(lambda a,b: int(a)+int(b),part.split()))
+            sumsV.append(val)
+        print(max(sumsV))
+        print(sum(sorted(sumsV)[-3:]))
+
+#practice inspired by someone elses solution        
+def solveMap():
+     with open("input.txt") as file:
+        data = file.read().strip().split("\n\n")
+        sumsV = []
+        for part in data:
+            val = map(int, part.split())
+            sumsV.append(sum(val))
+        print(max(sumsV))
+        print(sum(sorted(sumsV)[-3:]))
+
+
 if __name__ == "__main__":
-     inputPath = "./Day1/input.txt"
+     inputPath = "input.txt"
      listInput = readFile(inputPath)
      maxCalorie = getMax(listInput)
-     print("---Part One --- ")
+     print("---Part One--- ")
      print(maxCalorie)
 
      maxCalorie = getThreeMax(listInput)
