3ae97d493bc434953533ce5907f5f396218bf2d2
Tom P <tomp8711@gmail.com>
2022-12-07 09:20:40 -0500

    day 7

--- 2022/Day7/main.py
+++ 2022/Day7/main.py
@@ -1,7 +1,5 @@
 #DAY 7 No Space Left On Device
 
-import copy
-
 with open("input.txt") as file:
     data = file.read().split("\n")
     totals = {}
@@ -9,7 +7,6 @@ with open("input.txt") as file:
     order = []
     currentKey = ""
     i = 0
-    count = "0"
     while(i < len(data)):
         if "cd " in data[i] and "cd .." not in data[i]:
             currentKey = currentKey +  data[i].split()[2]
@@ -18,18 +15,17 @@ with open("input.txt") as file:
             nestedLetter.update({currentKey: list()})
             i = i+1
         elif "ls" in data[i] and "$" in data[i]:
-            i = i+1
-        
+            continue        
         elif "dir " in data[i]:
             nestedLetter[currentKey].append(currentKey + data[i].split()[1])
-            i = i+1
         elif "cd .." in data[i] and "$" in data[i]:
             order.pop()
             currentKey = order[-1]
-            i = i + 1
+           
         else:
             totals[currentKey] += int(data[i].split()[0])
-            i = i+1
+
+        i = i+1   
     
 
     keys = list(nestedLetter.keys())
